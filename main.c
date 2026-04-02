a552f898f2a1e1fc7e1e7aaa4e6c3b30d2562758
IanAvi15 <icaviles20@hotmail.com>
2026-04-02 15:22:48 -0400

    changed author name

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Char: %c\n", 'A');
+    _printf("String: %s\n", "Hello");
+    _printf("Int: %d\n", 1234);
+    _printf("Percent: %%\n");
+
+    return 0;
+}
